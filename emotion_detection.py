fdaae2265b6673b06d2128011593a392086d91c1
wjf37 <vincentw3007@gmail.com>
2025-07-18 11:29:29 -0400

    Task 3 done

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,5 +8,8 @@ def emotion_detector(text_to_analyze):
     INPUT_JSON = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(URL, json=INPUT_JSON, headers=HEADERS, timeout=10)
+    formatted_res = json.loads(response.text)
 
-    return response.text
+    emotions = formatted_res["emotionPredictions"][0]["emotion"]
+    emotions["dominant_emotion"] = max(emotions, key=emotions.get)
+    return emotions
